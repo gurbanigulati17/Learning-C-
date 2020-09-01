eb74bb9375c42825e45fdc257d7d85a9c1eb9bda
Gurubani Gulati <gurbanigulati17@gmail.com>
2020-09-01 22:54:55 +0530

    Add files via upload

---
+++ Program-8  Numbers and Integer Precision/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+
+    class Program
+    {
+ 
+
+  
+           static void Main(string[] args)
+        {  
+           //Getting Quotient
+           int a=7;
+           int b=4;
+           int c=3;
+
+           int d=(a+b)/c;
+           Console.WriteLine(d);
+
+           //Getting Remainder
+           int r=(a+b)%c;
+           Console.WriteLine(r);
+
+
+           //int MinValue Function and the MaxValue Function
+           //MinValue function gives the minimum
+           //MaxValue Function gives the maximum
+
+           int max=int.MaxValue;
+           int min=int.MinValue;
+           Console.WriteLine($"Min and max Values are {min} and {max}");
+           
+           int  what=min+3;
+           Console.WriteLine("Overflow values "+ what);
+
+
+
+           //double data type
+           double v=1.0/3.0;
+           Console.WriteLine("Value is "+v);
+           
+           
+        }
+    }
+
