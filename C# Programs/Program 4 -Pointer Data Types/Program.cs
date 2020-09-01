0089d28e80f59fb6073eec1108047a841413ce0b
Gurubani Gulati <gurbanigulati17@gmail.com>
2020-09-01 20:33:50 +0530

    Add files via upload

---
+++ C# Programs/Program 4 -Pointer Data Types/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+    class Program
+    {
+ 
+
+    static void Main(string[] args)
+        {
+            // Three category of data types : Namely;
+            //1. Value Data Types
+            //2. Referenced Data Type
+            //3. Pointer Data Types......................................
+
+            //In this program we ll concentrate highly on Pointer  Data Types.
+          
+            /*The Pointer Data Types will contain a memory address of the variable value.
+              To get the pointer details we have a two symbols ampersand (&) and asterisk (*).
+              ampersand (&): It is Known as Address Operator. It is used to determine the address of a variable.
+              asterisk (*): It also known as Indirection Operator. It is used to access the value of an address.*/
+
+
+          unsafe{
+
+          
+            // declare variable 
+            int n = 30; 
+              
+            // store variable n address  
+            // location in pointer variable p 
+            int*p = &n; 
+            Console.WriteLine("Value :{0}", n); 
+            Console.WriteLine("Address :{0}", (int)p); 
+     
+
+          }
+           
+        }
+    }
+
